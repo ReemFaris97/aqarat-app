2b0154a662db8cc7daa9163ee9db61abb5ea3d9b
abdullahessam <body12324@gmail.com>
2021-08-05 16:52:59 +0200

    fix pie chart

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     {
         $data=[
             'orders_category'=>\DB::table('orders')->join('categories','categories.id','=','orders.category_id')->selectRaw('count(orders.id) as orders , categories.name as category_name')->groupBy('categories.name')->get()->map(function ($q){
-                return ['label'=>$q->category_name,'value'=>$q->orders];
+                return ['label'=>object_get(json_decode($q->category_name),'ar','تصنيف'),'value'=>$q->orders];
             })->toArray(),
 
             'orders_in_day'=>Order::selectRaw('count(id) as orders , DATE(created_at) as date_created_at')->groupBy('date_created_at')->get()->toArray(),
--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -56,7 +56,7 @@ class UsersController extends Controller
     {
         $data=[
             'orders_category'=>$user->orders()->join('categories','categories.id','=','orders.category_id')->selectRaw('count(orders.id) as orders , categories.name as category_name')->groupBy('categories.name')->get()->map(function ($q){
-                return ['label'=>$q->category_name,'value'=>$q->orders];
+                return ['label'=>object_get(json_decode($q->category_name),'ar','تصنيف'),'value'=>$q->orders];
             })->toArray(),
 
             'orders_in_day'=>Order::where('user_id',$user->id)->selectRaw('count(id) as orders , DATE(created_at) as date_created_at')->groupBy('date_created_at')->get()->toArray(),
