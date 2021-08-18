b7bc381cfee2d0177d01cc93eb81bd836603364f
abdullahessam <body12324@gmail.com>
2021-08-18 14:32:30 +0200

    fix counters

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,22 +5,8 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$blog->title}}</title>
-    <style>
-        html, body {
-            height: 100%;
-            margin: 0;
-            padding: 10px;
-        }
-        img {
-            padding: 0;
-            display: block;
-            margin: 0 auto;
-            max-height: 100%;
-            max-width: 100%;
-            height: auto !important;
-            width: auto !important;
-        }
-    </style>
+    <link href="{{asset('webview/blog.css')}}">
+
 </head>
 <body>
 {!! $blog->description !!}
