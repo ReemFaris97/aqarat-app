d0c2b0221ee8048d7a09e9ff70ee39aea7efa286
fatma123 <fabdelrahman72@yahoo.com>
2021-08-23 11:01:48 +0200

    un comment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 //       $this->call(SettingSeeder::class);
         $this->call(SettingsTableSeeder::class);
-//        $this->call(PermissionTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
 //        $this->call(RolesTableSeeder::class);
 //        $this->call(AdminSeeder::class);
 //        $this->call(UserSeeder::class);
